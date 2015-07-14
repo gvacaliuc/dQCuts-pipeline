a157126ee46d713839710e70df36437c97cca1cf
gvacaliuc <gabe.vacaliuc@gmail.com>
2015-07-14 11:13:55 -0400

    changed for new repo

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir savefiles;
 git clone https://github.com/gvacaliuc/wqaa.git;
-git clone https://github.com/gvacaliuc/dncuts-eigensolver.git;
+git clone https://github.com/gvacaliuc/dncuts_eigensolver.git;
 echo "Directory has been intialized.  You may now run 'main.py'";
